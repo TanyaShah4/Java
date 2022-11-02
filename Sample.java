48c49f51fc3dca6a9d07f3fae40fd40761266393
root <root@ip-172-31-33-18.ap-south-1.compute.internal>
2022-11-02 17:56:30 +0000

    f2

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String args[]){
 
-System.out.println("Hello my name is Tanya Shah cloud");
+System.out.println("Hello my name is Tanya Shah and cloud technology fascinates me");
 }
 }
